47d5bc6dbf6ec69da1734793c366965910fabc90
KDEE247 <kdogidan@gmail.com>
2023-08-23 07:14:03 -0700

    changes

--- functions2.c
+++ functions2.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /* PRINT POINTER */
 /**
- * print_pointer - prints value of a pointer variable.
+ * print_pointer - Prints the value of a pointer variable.
  * @types: list of arguments.
- * @buffer: buffer array to handle print.
+ * @buffer: Buffer array to handle print.
  * @flags: calculates active flags.
  * @width: get Width.
  * @precision: Precision specification.
- * @size: Size specifier.
+ * @size: Size specifier,
  * Return: number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
@@ -53,9 +54,9 @@ int print_pointer(va_list types, char buffer[],
 
 /* PRINT NON PRINTABLE */
 /**
- * print_non_printable - prints Ascii codes in hexa of non printable chars.
+ * print_non_printable - Prints Ascii codes in hexa of non printable chars
  * @types: list of arguments.
- * @buffer: buffer array to handle print.
+ * @buffer: Buffer array to handle print.
  * @flags: calculates active flags.
  * @width: get Width.
  * @precision: Precision specification.
@@ -93,11 +94,11 @@ int print_non_printable(va_list types, char buffer[],
 
 /* PRINT REVERSE */
 /**
- * print_reverse - prints reverse string.
+ * print_reverse - Prints reverse string.
  * @types: list of arguments.
- * @buffer: buffer array to handle print.
+ * @buffer: Buffer array to handle print.
  * @flags: calculates active flags.
- * @width: get Width.
+ * @width: get Width
  * @precision: Precision specification.
  * @size: Size specifier.
  * Return: number of chars printed.
@@ -136,10 +137,10 @@ int print_reverse(va_list types, char buffer[],
 }
 /* PRINT A STRING IN ROT13 */
 /**
- * print_rot13string - print a string in rot13.
+ * print_rot13string - Prints a string in rot13.
  * @types: list of arguments.
- * @buffer: buffer array to handle print.
- * @flags: calculates active flags.
+ * @buffer: Buffer array to handle print.
+ * @flags: calculates active flags
  * @width: get Width.
  * @precision: Precision specification.
  * @size: Size specifier.
--- utils.c
+++ utils.c
@@ -36,7 +36,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit -vVerifies if a char is a digit.
+ * is_digit verifies if a char is a digit.
  * @c: Char to be evaluated.
  * Return: 1 if c is a digit, 0 otherwise.
  */
