2716fac1ee9979008213661491171482da66e464
KDEE247 <oyoriesther@gmail.com>
2023-08-25 13:16:42 -0700

    0x11. C - printf

--- main.h
+++ main.h
@@ -144,7 +144,4 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
-<<<<<<< HEAD
 
-=======
->>>>>>> 47d5bc6dbf6ec69da1734793c366965910fabc90
